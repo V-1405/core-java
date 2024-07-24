fc9747f6f73127d2d9f801de8de6e73da29b3bb1
v-1405 <vedhashree.xworks@gmail.com>
2024-07-24 10:00:48 +0530

    constructur

---
+++ Helicopter.java
@@ -0,0 +1,120 @@
+class Helicopter{
+	
+	Helicopter()
+	{
+		System.out.println("Created Helicopter");
+	}
+}
+
+class Market{
+	
+	Market()
+	{
+		System.out.println("Created Market");
+	}
+}
+
+class Mall{
+	
+	Mall()
+	{
+		System.out.println("Created Mall");
+	}
+}
+
+class Train{
+	
+	Train()
+	{
+		System.out.println("Created Train");
+	}
+}
+
+class Ring{
+	
+	Ring()
+	{
+		System.out.println("Created Ring");
+	}
+}
+
+class Auto{
+	
+	Auto()
+	{
+		System.out.println("Created Auto");
+	}
+}
+
+class Auditorium{
+	
+	Auditorium()
+	{
+		System.out.println("Created Auditorium");
+	}
+}
+
+class SpaceShip{
+	
+	SpaceShip()
+	{
+		System.out.println("Created SpaceShip");
+	}
+}
+
+class Alien{
+	
+	Alien()
+	{
+		System.out.println("Created Alien");
+	}
+}
+
+class Bullet{
+	
+	Bullet()
+	{
+		System.out.println("Created Bullet");
+	}
+}
+
+class Galaxy{
+	
+	Galaxy()
+	{
+		System.out.println("Created Galaxy");
+	}
+}
+
+class Spoon{
+	
+	Spoon()
+	{
+		System.out.println("Created Spoon");
+	}
+}
+
+class BlachHole{
+	
+	BlachHole()
+	{
+		System.out.println("Created BlachHole");
+	}
+}
+
+class Cake{
+	
+	Cake()
+	{
+		System.out.println("Created Cake");
+	}
+}
+
+class Wrapper{
+	
+	Wrapper()
+	{
+		System.out.println("Created Wrapper");
+	}
+}
+
---
+++ HelicopterRunner.java
@@ -0,0 +1,154 @@
+class HelicopterRunner{
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Start Helicopter in HelicopterRunner");
+		Helicopter helicopter1=new Helicopter();
+		Helicopter helicopter2=new Helicopter();
+		Helicopter helicopter3=new Helicopter();
+		Helicopter helicopter4=new Helicopter();
+		Helicopter helicopter5=new Helicopter();
+		System.out.println("End main in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start Market in HelicopterRunner");
+		Market market1=new Market();
+		Market market2=new Market();
+		Market market3=new Market();
+		Market market4=new Market();
+		Market market5=new Market();
+		System.out.println("End Market in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start Mall in HelicopterRunner");
+		Mall mall1=new Mall();
+		Mall mall2=new Mall();
+		Mall mall3=new Mall();
+		Mall mall4=new Mall();
+		Mall mall5=new Mall();
+		System.out.println("End Mall in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start Train in HelicopterRunner");
+		Train train1=new Train();
+		Train train2=new Train();
+		Train train3=new Train();
+		Train train4=new Train();
+		Train train5=new Train();
+		System.out.println("End Train in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start Ring in HelicopterRunner");
+		Ring ring1=new Ring();
+		Ring ring2=new Ring();
+		Ring ring3=new Ring();
+		Ring ring4=new Ring();
+		Ring ring5=new Ring();
+		System.out.println("End Ring in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start Auto in HelicopterRunner");
+		Auto auto1=new Auto();
+		Auto auto2=new Auto();
+		Auto auto3=new Auto();
+		Auto auto4=new Auto();
+		Auto auto5=new Auto();
+		System.out.println("End Auto in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start Auditorium in HelicopterRunner");
+		Auditorium auditorium1=new Auditorium();
+		Auditorium auditorium2=new Auditorium();
+		Auditorium auditorium3=new Auditorium();
+		Auditorium auditorium4=new Auditorium();
+		Auditorium auditorium5=new Auditorium();
+		System.out.println("End Auditorium in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start SpaceShip in HelicopterRunner");
+		SpaceShip spaceship1=new SpaceShip();
+		SpaceShip spaceship2=new SpaceShip();
+		SpaceShip spaceship3=new SpaceShip();
+		SpaceShip spaceship4=new SpaceShip();
+		SpaceShip spaceship5=new SpaceShip();
+		System.out.println("End SpaceShip in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start Alien in HelicopterRunner");
+		Alien alien1=new Alien();
+		Alien alien2=new Alien();
+		Alien alien3=new Alien();
+		Alien alien4=new Alien();
+		Alien alien5=new Alien();
+		System.out.println("End Alien in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start Bullet in HelicopterRunner");
+		Bullet bullet1=new Bullet();
+		Bullet bullet2=new Bullet();
+		Bullet bullet3=new Bullet();
+		Bullet bullet4=new Bullet();
+		Bullet bullet5=new Bullet();
+		System.out.println("End Bullet in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start Galaxy in HelicopterRunner");
+		Galaxy galaxy1=new Galaxy();
+		Galaxy galaxy2=new Galaxy();
+		Galaxy galaxy3=new Galaxy();
+		Galaxy galaxy4=new Galaxy();
+		Galaxy galaxy5=new Galaxy();
+		System.out.println("End Galaxy in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start Spoon in HelicopterRunner");
+		Spoon spoon1=new Spoon();
+		Spoon spoon2=new Spoon();
+		Spoon spoon3=new Spoon();
+		Spoon spoon4=new Spoon();
+		Spoon spoon5=new Spoon();
+		System.out.println("End Spoon in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start BlachHole in HelicopterRunner");
+		BlachHole blachhole1=new BlachHole();
+		BlachHole blachhole2=new BlachHole();
+		BlachHole blachhole3=new BlachHole();
+		BlachHole blachhole4=new BlachHole();
+		BlachHole blachhole5=new BlachHole();
+		System.out.println("End BlachHole in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start Cake in HelicopterRunner");
+		Cake cake1=new Cake();
+		Cake cake2=new Cake();
+		Cake cake3=new Cake();
+		Cake cake4=new Cake();
+		Cake cake5=new Cake();
+		System.out.println("End Cake in HelicopterRunner");
+		
+		System.out.println("====================================================");
+		
+	    System.out.println("Start Wrapper in HelicopterRunner");
+		Wrapper wrapper1=new Wrapper();
+		Wrapper wrapper2=new Wrapper();
+		Wrapper wrapper3=new Wrapper();
+		Wrapper wrapper4=new Wrapper();
+		Wrapper wrapper5=new Wrapper();
+		System.out.println("End Wrapper in HelicopterRunner");
+		
+	}
+}
